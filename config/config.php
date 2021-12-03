bb13d91d2e2a9798fb954a3afa2a364daf850518
kumars35 <kumars35@mcmaster.ca>
2021-12-04 05:10:18 +0530

    file updated

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 	// Database credentials
-	define('DB_SERVER', 'localhost');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', '');
-	define('DB_NAME', 'login_system');
+	define('DB_SERVER', 'sql6.freesqldatabase.com');
+	define('DB_USERNAME', 'sql6456226');
+	define('DB_PASSWORD', '8FSZa1P9gF');
+	define('DB_NAME', 'sql6456226');
 
 	// Attempt to connect to MySQL database
 	$mysql_db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
