d2091f196110fb8b48ca659cd159fafd60d2f9f4
kumars35 <kumars35@mcmaster.ca>
2021-12-04 07:13:00 +0530

    Update

--- config/config.php
+++ config/config.php
@@ -1,11 +1,9 @@
 <?php
 	// Database credentials
-
-	define('DB_SERVER', 'ww3-database.cp0wobp38lvg.us-east-2.rds.amazonaws.com');
-	define('DB_USERNAME', 'admin');
-	define('DB_PASSWORD', 'adminadmin');
-	define('DB_NAME', 'WW3_database');
-
+	define('DB_SERVER', 'sql6.freesqldatabase.com');
+	define('DB_USERNAME', 'sql6456226');
+	define('DB_PASSWORD', '8FSZa1P9gF');
+	define('DB_NAME', 'sql6456226');
 
 	// Attempt to connect to MySQL database
 	$mysql_db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
